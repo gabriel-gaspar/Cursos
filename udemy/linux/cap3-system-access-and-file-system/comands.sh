83df012a43447b927a2d4a448c52ef889edc5ece
gabriel-gaspar <gabrielgaspar@wine.com.br>
2020-12-29 17:39:53 -0300

    adiciona histórico de comandos aprendidos até a aula 42

---
+++ udemy/linux/cap3-system-access-and-file-system/comands.sh
@@ -0,0 +1,26 @@
+# Comando aprendidos neste capítulo
+
+# Utilitários
+
+# exibe usuario atual logado no sistema
+whoami
+
+# trocar de usuário
+su
+su - # trocar para o usuário root
+
+clear # limpa terminal
+
+# Rede ----------------------------------------------------------
+ifconfig # Lista interfaces de rede
+ip addr # Lista interfaces de rede
+ip a # Lista interfaces de rede
+
+# Sistema de arquivos --------------------------------------------
+# cd - change directory
+cd /folderName
+#pwd - print working directory
+pwd
+# ls - list
+ls  # lista o conteúdo de um diretório
+ls -l # lista e detalha o conteúdo de um diretório
