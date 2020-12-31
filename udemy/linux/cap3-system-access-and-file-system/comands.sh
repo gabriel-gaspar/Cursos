3569e6ed51f74670308554de019171c3ab2c4398
gabriel-gaspar <gabrielgaspar@wine.com.br>
2020-12-30 22:15:02 -0300

    adiciona comandos das aulas 60 a 67

--- udemy/linux/cap3-system-access-and-file-system/comands.sh
+++ udemy/linux/cap3-system-access-and-file-system/comands.sh
@@ -34,6 +34,7 @@ pwd
 ls  # lista o conteúdo de um diretório
 ls -l # lista e detalha o conteúdo de um diretório
 ls -ltr # lista com detalhes, ordena por tempo em ordem reversa
+ls -ltr | more #  comando com paginação (por causa do more)
 ls -l *[xy]* # lista arquivos que satisfizerem o wildcard
 ls -l * xy * # lista arquivos que satisfizerem o wildcard
 ls -l ?bcd* # lista arquivos que satisfizerem o wildcard
@@ -41,12 +42,42 @@ ls -l *xyz # lista arquivos que satisfizerem o wildcard
 ls -l abc* # lista arquivos que satisfizerem o wildcard
 
 
+# Manipulação de arquivos e textos
 touch gaspar.txt   # cria um arquivo
 rm -f gaspar.txt   # remove um arquivo
-rm -r folderName   # remove uma pasta
+rm -Rf folderName   # remove uma pasta
 touch arquivo[1..9]-xyz # cria arquivos seguindo o padrão do wildcard
 cp ./gaspar.txt ./gaspar_backuo.txt  #faz uma cópia do arquivo
 mkdir documentos # cria um diretório
+mv gaspar.txt gabriel.txt # renomeia o arquivo
+mv gabriel.txt /tmp # move um arquivo
+
+echo "hello world" > hellofile
+echo "hello again" >> hellofile
+cat hellofile
+more logFile # uma página por vez do arquivo no terminal
+less logFile # uma página por vez do arquivo no terminal em ordem reversa
+head -5 logFile # as 5 primeiras linhas do arquivo
+tail -5 logFile # as 5 ultimas linhas do arquivo
+touch fileWithOutput
+ls -ltr > fileWithOutput # escreve a saída de um comando em um arquivo
+ls -ltr | tee fileWithOutput # exibe a saída e escreve ela em um arquivo
+echo "tee command and apend option" | tee -a fileWithOutput
+
+cut -c1 filename # de cada linha do arquivo pega o primeiro caractere
+cut -c1,2,4 filename # de cada linha do arquivo pega os caracteres 1, 2 e 4
+cut -c1-3 filename # de cada linha do arquivo pega os caracteres de 1 a 3
+cut -c1-3,6-8 filename # de cada linha do arquivo, pega os caracteres de 1 a 3 e de 6 a 8
+cut -d: -f 6,7 /etc/passwd # em um arquivo com campos delimitados por ":", de cada linha extrai os 6º e 7º campos
+ls -ltr | cut -c40-51 | grep dez # EU QUE FIZZZ \o/
+# O comando awk considera o espaço como delimitador padrão
+ls -ltr | awk '{print $6,$7,$8}' # EU QUE FIZZ, faz a mesma coisa que o comando anterior
+ls -ltr | awk '{print $NF}' # retorna a ultima coluna da saída do comando ls
+# Para usar um delimitador diferente, como "":""
+awk -F: '{print $6,$7}' /etc/passwd
+# O comando abaixo vai trocar o segundo campo por Gaspar e vai printar o resultado
+# $0 significa printar todos os campos
+echo "Hello Gabriel" | awk '{$2="Gaspar"} ; print $0'
 
 # Busca de arquivos (retornam o caminho do arquivo, caso seja encontrado)
 find . -name gaspar.txt # a partir do caminho atual, procura pelo arquivo
@@ -79,9 +110,4 @@ setfacl -m g:groupName:rw path/to/file
 setfacl -dm "rwx" /path/to/file
 setfacl -b /path/to/file
 
-# Manipulação de arquivos e textos
-echo "hello world" > hellofile
-echo "hello again" >> hellofile
-cat hellofile
-touch fileWithOutput
-ls -ltr > fileWithOutput # escreve a saída de um comando em um arquivo
+
