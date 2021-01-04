3d5eb3a37485aa23392fbd38f3136e32d3c3a47f
gabriel-gaspar <gabrielgaspar@wine.com.br>
2021-01-04 18:14:46 -0300

    adiciona comandos das aulas 68 a 75

--- udemy/linux/comands.sh
+++ udemy/linux/comands.sh
@@ -78,7 +78,41 @@ awk -F: '{print $6,$7}' /etc/passwd
 # O comando abaixo vai trocar o segundo campo por Gaspar e vai printar o resultado
 # $0 significa printar todos os campos
 echo "Hello Gabriel" | awk '{$2="Gaspar"} ; print $0'
+aws 'length($0)>15' listaDeChamada # retorna apenas os nomes que possuem mais de 15 caracteres
+ls -l | awk '{if($NF == "listaDeChamada") print $0;}' # retorna apenas linha cuja ultima coluna vale listaDeChamada
+ls -ltr | awk '{print $1}' | cut -c1 | awk '{if($0 == "-") print $0;}' | wc -w # EU QUE FIZZZ, conta a quantidade de arquivos no diretório atual
+ls -ltr | awk '{print $1}' | cut -c1 | awk '{if($0 == "d") print $0;}' | wc -w # conta a quantidade de diretórios no diretório atual
 
+
+grep gaspar listaDeChamada # vai buscar todas as linhas que contenham a palavra gaspar
+grep -c gaspar listaDeChamada # retorna a quantidade de ocorrencias do comando acima
+grep -i GasPAR listaDeChamada # ignorar letras maiusculas e minusculas na busca
+grep -n gaspar listaDeChamada # busca as linhas que contenham a palvra gaspar e informa o numero da linha no resultado
+grep -vi gaspar listaDeChamada # retorna tudo menos as linhas que contiverem a palavra gaspar
+egrep -i "GasPaR|Luzia" listaDeChamada # mesma coisa que o grep, mas busca mais de uma keyword
+
+wc listaDeChamada  # retorna n° linhas/words/bytes
+wc -l listaDeChamada # retorna n° de linhas
+wc -w listaDeChamada # retorna n° palavras
+wc -c listaDeChamada # retorna n° de bytes
+
+# Comprimindo arquivos de uma pasta
+tar cvf nomeDoContainer.tar /path/to/folder # comprime o conteúdo de uma pasta para um arquivo .tar
+tar xvf nomeDoContainer.tar # descompactar o conteúdo no diretório atual
+gzip nomeDoContainer.tar # gera um arquivo ainda mais compactado, no formato tar.gz
+gzip -d nomeDoContainer.tar.gz # descompacta o arquivo .tar.gz de voltar para o arquivo .tar
+
+# Recortando arquivos
+# Esse comando vai reduzir o arquivo para 40 bytes. Haverá perda de conteúdo.
+truncate -s 40 nomeDoArquivo
+
+# Combinando arquivos
+cat lista1 lista2 > lista3
+# Dividindo arquivos
+# a lista vai ser dividida em vários arquivos menores, cada 1 contendo 2 linhas da lista original
+# sublista é o prefixo que cada arquivo vai ter, seguido de um sufixo de ordenação (a,b,c,d etc...)
+split -l 2 listaDePaises sublista 
+                
 # Busca de arquivos (retornam o caminho do arquivo, caso seja encontrado)
 find . -name gaspar.txt # a partir do caminho atual, procura pelo arquivo
 find / -name gaspar.txt # procura o arquivo a partir da raíz do sistema (necessário acesso root)
